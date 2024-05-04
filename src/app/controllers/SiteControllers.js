672f961157744b44a53e939eadcc722139d47b99
nqba2509 <nqba2509@gmail.com>
2024-05-04 10:32:26 +0700

    coursedetail page

--- src/app/controllers/SiteControllers.js
+++ src/app/controllers/SiteControllers.js
@@ -1,13 +1,21 @@
 const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 class SiteController {
   // [GET] /news
-  async home(req, res) {
-    try {
-      const data = await Course.find({});
-      res.json(data);
-    } catch (res) {
-      res.status(400).json({ error: "ERROR!" });
-    }
+  home(req, res, next) {
+    // try {
+    //   const data = await Course.find({});
+    //   res.json(data);
+    // } catch (res) {
+    //   res.status(400).json({ error: "ERROR!" });
+    // }
+    Course.find({})
+      .then((courses) => {
+        res.render("home", {
+          courses: mutipleMongooseToObject(courses),
+        });
+      })
+      .catch(next);
   }
 
   // [GET] /search
@@ -17,3 +25,4 @@ class SiteController {
 }
 
 module.exports = new SiteController();
+``
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const engine = require("express-handlebars").engine;
 const route = require("./routes");
+
 const path = require("path");
 const app = express();
 const port = 3000;
-const db = require('./config/db')
 
+const db = require("./config/db");
 //Connect to BD
 db.connect();
 
@@ -24,12 +25,12 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //Routes init
 route(app);
 
 //Port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
